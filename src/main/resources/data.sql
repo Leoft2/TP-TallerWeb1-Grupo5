91bb1ab29c8868faf433e6cd666569dc78e56483
Mauricio Chambi <chambi7mauricio@gmail.com>
2026-06-03 17:19:42 -0300

    agregando usuario a data.sql para validar el incio-junior

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,3 +2,9 @@ INSERT INTO Usuario(id, email, password, rol, activo) VALUES(null, 'test@unlam.e
 INSERT INTO pregunta(lenguaje, dificultad, consigna, opciona, opcionb, opcionc, respuestacorrecta)VALUES('JAVA', 'BASICO','¿Qué palabra clave se utiliza para declarar una clase en Java?','class', 'public', 'void','A');
 
 INSERT INTO pregunta(lenguaje, dificultad, consigna, opciona, opcionb, opcionc, respuestacorrecta)VALUES('JAVA', 'BASICO','¿Cuál es el método principal de una aplicación Java?','run()', 'main()', 'start()','B');
+
+INSERT INTO usuario (nombreUsuario, email, password, rol, activo, puntaje)VALUES ('juanperez', 'juan@gmail.com', '1234', 'junior', true, 75);
+
+INSERT INTO usuario (nombreUsuario, email, password, rol, activo, puntaje)VALUES ('mariasosa', 'maria@gmail.com', '1234', 'junior', true, 82);
+
+INSERT INTO usuario (nombreUsuario, email, password, rol, activo, puntaje)VALUES ('carloslopez', 'carlos@gmail.com', '1234', 'reclutador', true, 0);
